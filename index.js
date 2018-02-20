ebacc0b2959465f4cb92be2935de9f227574510d
Lisa Chan <xlisachan@gmail.com>
2018-02-20 00:03:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
       return cart;
     }
   }
-  console.log("That item is not in your cart.");
+  console.log(`That item is not in your cart.`);
   return cart;
 }
 
